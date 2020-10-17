ce5d4b660848198a5a86c4bfc3d2e566deb415af
Leonidios <programertv633@gmail.com>
2020-10-17 14:04:36 +0300

    Extended response class

--- client_side/requests.py
+++ client_side/requests.py
@@ -1,9 +1,6 @@
 import socket,ssl,pprint
-from re import match
-from math import ceil
-# import ..parsing.http as parseql
+from time import time as current_time
 from ..parsing.http import ParseHeaders
-import zlib
 
 def parse_headers(data : bytes,):
     parsed : dict = ParseHeaders(data)
@@ -28,9 +25,8 @@ class Header:
     def c(self):
         value = ''
         for item in self.headers:
-            value+= f'{item}: {self.headers.get(item)}\r\n' # GAMO TO XRISTO SAS NGINX KAI APACHE MALAKES 
-        return value                                        # DEN MPOREITE NA KANATE PARSE TA HEADERS
-                                                            # AN IPARXEI GAMIMENO KENO PRIN TO :
+            value+= f'{item}: {self.headers.get(item)}\r\n' 
+        return value 
 
     def pop(self,value):
         return self.headers.pop(value)
@@ -47,15 +43,18 @@ class Header:
 class Response:
     
     def __init__(self,request_url,status_code,headers,**kwargs) -> None:
-        self.headers = headers
+        self.response_headers = headers
+        self.request_headers = kwargs.get("request_headers")
         self.status_code = status_code
         self.request_url = request_url
-        self.host = kwargs.get('host') # detail_response
+        self.host = kwargs.get('host') 
         self.detail_resp = kwargs.get("detail_response")
-
+        self.body = kwargs.get('body')
+        self.is_ssl = kwargs.get("ssl")
+        self.time_taken = kwargs.get('time_taken')
 
     def __str__(self) -> str:
-        return f'<XMLHttpRequest [{self.status_code}] alias="{self.detail_resp}" target="{self.host}" header_length="{len(self.headers)}" />'
+        return f'<XMLHttpRequest [{self.status_code}] alias="{self.detail_resp}" host="{self.host}" timing="{round(self.time_taken*1000,2)}ms" SSL_ENCRYPTED={self.is_ssl} />'
 
     def setBody(self, data : bytes) -> None:
         self.body = data
@@ -100,7 +99,7 @@ base_request = lambda **kwargs: Header(
     },
     {
         "Host" : kwargs.get("host"),
-        "Accept-Encoding": "gzip, deflate, identity",
+        "Accept-Encoding": "identity",
         "Accept-Language": "en-US,en;q=0.9",
 })
 
@@ -116,6 +115,7 @@ def XMLHttpRequest(headers : Header,recv_size : int = 1024,
     (host,port),path = get_host(headers.get_url())
     headers.url = path
     headers.headers['Host'] = host
+    # TODO ---> ADD LIMIT FOR HEADERS AND BODY <--- TODO
     try:
         with socket.create_connection((host,port)) as connection:
             if(port!=443):
@@ -125,8 +125,8 @@ def XMLHttpRequest(headers : Header,recv_size : int = 1024,
                 h_b = response.split(2*b'\r\n',1)
                 return parseHRS(h_b,headers,host,recv= lambda : connection.recv(1024))
 
+            t0 = current_time()
             with ssl.create_default_context().wrap_socket(connection,server_hostname=host) as secure_connection:
-                print(headers())
                 secure_connection.send(headers().encode())
                 secure_connection.settimeout(max_wait_time)
                 hedrs : bytes = b''
@@ -139,45 +139,40 @@ def XMLHttpRequest(headers : Header,recv_size : int = 1024,
 
                     spl = response.split(b"\r\n\r\n")
                     if(len(spl) > 1):
+                        body += spl[1]
                         hedrs += spl[0]
                         hedrs = parse_headers(hedrs)
                         break
 
                     hedrs += response
                 
-                if(hedrs.get('status').startswith('30')): # 3xx Redirection
+                if(hedrs.get('status').startswith('30')): # 3xx Redirection - No Request Body
                     location = hedrs.get("Location")
-                    if(not allow_redirect or not location):
-                        return "TELOS " # TODO BUG NOTE
-                    kwargs["headers"].url = location
-                    if(not allow_recursive_redirect):
-                        kwargs['allow_redirect'] = False
-                    pprint.pprint(hedrs)
-                    return XMLHttpRequest(**kwargs)
-                while True: # Parse Body
-                    try:
-                        data = secure_connection.recv(recv_size)
-                        body += data
-                        print(data)
-                        if(data.strip() == b'0'):
+                    if(allow_redirect and location):
+                        kwargs["headers"].url = location
+                        if(not allow_recursive_redirect):
+                            kwargs['allow_redirect'] = False
+                        return XMLHttpRequest(**kwargs)
+                else:    
+                    while True: # Response in 1xx - 2xx - 4xx - 5xx
+                        try:
+                            data = secure_connection.recv(recv_size)
+                            body += data
+                            if(data.strip() == b'0'):
+                                break
+                        except:
                             break
-                    except:
-                        break
-
+        t1 = current_time()
     except socket.gaierror:
         raise RuntimeError(f"No results on DNS lookup for '{host}' on port {port}.")
 
-    return hedrs,body
-    # return parseHRS(h_b,headers,host,recv= lambda : secure_connection.recv(1024))
+    return Response(headers=hedrs,status_code=hedrs.pop('status'),host=host,request_url=path,
+    detail_response=hedrs.pop("alias"),body=body,ssl=True,time_taken=(t1-t0),request_headers=headers.headers)
+    # return hedrs,body
             
 def GET(target,**kwargs):
     return XMLHttpRequest(base_request(url=target,method='GET'),**kwargs)
 
 if __name__ == "__main__":
-    pprint.pprint(
-        GET('https://www.youtube.com/watch?v=OOZjzq_e7bA',max_wait_time=3,allow_redirect=True,allow_recursive_redirect=True)[0]
-    )
-    # req = XMLHttpRequest(request)
-    # print(req)
-    # pprint.pprint(req.headers)
-    
+    response = GET('https://bourdela.com',max_wait_time=3,allow_redirect=False)
+    print(response)
