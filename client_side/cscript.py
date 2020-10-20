3e92a5c6fa8b4614816082497c823569b9bfec18
Leonidios <programertv633@gmail.com>
2020-10-20 21:49:33 +0300

    New Python in-html parsing

--- client_side/cscript.py
+++ client_side/cscript.py
@@ -7,6 +7,7 @@ functions inside of here will 'compile' the scripts
 
 import re
 from typing import Tuple
+from ..server.server import lazy_read
 
 
 def popAll(item):
@@ -59,47 +60,51 @@ def findScript(html,content,compiled : Tuple[bool,str] = (False,'')):
 if __name__ == "__main__":
     from io import StringIO
 
-    def lazy_read(file): #Function to lazy read
-        while True:
-            data = file.read(1024)
-            if not data:
-                break
-            yield data
+    def replace_str_index(text,index=0,replacement=''):
+        return '%s%s%s'%(text[:index],replacement,text[index+1:])
 
-    if 2 != 1 + 1 - 0:
-        with open(path,'r',encoding='utf-8') as f:
-            is_wait = False
-            statement = StringIO()
-            for chunk in lazy_read(f):
-                if(is_wait):
-                    try:
-                        indx2 = chunk.index("?>")
-                        statement.write(chunk[:indx2+2])
-                        is_wait = False
-                        print("<-------- ---------->")
-                        print("Statement is")
-                        print(statement.getvalue())
-                        print("<-------- ---------->")
-                        print(chunk[indx2+2:])
-                    except:
-                        statement.write(chunk)
-                        continue
+    REGXP = r'\((\s+)?<\w+(.*?)>(.*?)</\w+>(\s+)?\)'
+
+    def eval_script(data):
+        html : list = []
+        start = data.index('<?python')
+        print(data[:start])
+        end = data.index('?endpython>') + len("?endpython>")
+        point_of_interest = data[start+len("<?python"):end-len("?endpython>")]
+        point_of_interest = "if 1:" + point_of_interest # \(<\w+>(.*?)</\w+>\)
+
+        for item in (re.finditer(REGXP,point_of_interest,flags=re.DOTALL)):  # r'\(<\w+>(.*?)</\w+>\)'
+            match = item.group()
+            point_of_interest = point_of_interest.replace(match,r'html.append(f"""' + replace_str_index(replace_str_index(match),len(match)-2) + r'""")')
+
+        exec(point_of_interest)
+        print("\n".join(html))
+        print(data[end:])
+    
+
+    def script_in_chunks(file, buffer_size : int = 1024):
+        prev = StringIO()
+
+        def find_script(buff_chunk,check_buffer : bool = True):
+            try:
+                start = buff_chunk.index('<?python')
                 try:
-                    indx1 = chunk.index("<?python")
-                    print(chunk[:indx1])
-                    try:
-                        indx2 = chunk.index("?>")
-                        s = chunk[indx1:indx2+2]
-                        print("<-------- ---------->")
-                        print("Statement is")
-                        print(s)
-                        print("<-------- ---------->")
-                        print(chunk[:indx2+2])
-                    except:
-                        s = chunk[indx1:]
-                        is_wait = True
-                        statement.write(s)
-                        pass
+                    end = buff_chunk.index("?endpython>")
+                    return [buff_chunk[:start],eval_script(buff_chunk[start:end]),buff_chunk[end:]]
                 except:
-                    print(chunk)
-                    continue
+                    return [buff_chunk[:start],buff_chunk[start:]]
+            except:
+                return buff_chunk
+
+    
+        with open(file) as f:
+            for chunk in lazy_read(f,buffer_size):
+                _buffer : StringIO = StringIO()
+                pars = find_script(chunk)
+                if(len(pars)==2):
+                    print(pars[0])
+                    _buffer.write(pars[1])
+                elif(len(pars)==3):
+                    print(pars[0])
+                    print(pars[1])
+                    pass
